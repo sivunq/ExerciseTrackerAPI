feb6833152c16bd4853532973a19e5316c013f03
Navdeep <10390511+sivunq@users.noreply.github.com>
2020-10-05 20:35:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://navdeep:navdeep_786_MB@cluster0-b6oxb.mongodb.net/test?retryWrites=true&w=majority';
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
 app.get('/', (req, res) => {
 	console.log("here");
     res.json({"message": "Welcome to Exercise Tracker"});
