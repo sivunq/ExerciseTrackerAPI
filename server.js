e60195434dd24f1f624b89a3604b62f6b69a7083
Navdeep <10390511+sivunq@users.noreply.github.com>
2020-10-05 20:28:12 +0530

    updating URI

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://navdeep:navdeep_786_MB@cluster0-b6oxb.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  }
 );
 const connection = mongoose.connection;
